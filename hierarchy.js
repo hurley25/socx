48cab6d3889dfd3ba710d3d59aa97829640f8ef6
hurley25 <liuhuan1992@gmail.com>
2015-12-31 14:35:16 +0800

    update doc
    Signed-off-by: hurley25 <liuhuan1992@gmail.com>

--- hierarchy.js
+++ hierarchy.js
@@ -6,8 +6,8 @@ var hierarchy =
       [ "socx::util::BlockingQueue< T >", "classsocx_1_1util_1_1_blocking_queue.html", null ],
       [ "socx::util::CountDownLatch", "classsocx_1_1util_1_1_count_down_latch.html", null ],
       [ "socx::util::Logger", "classsocx_1_1util_1_1_logger.html", null ],
-      [ "socx::util::ThreadPool", "classsocx_1_1util_1_1_thread_pool.html", null ]
+      [ "socx::util::ThreadPool", "classsocx_1_1util_1_1_thread_pool.html", null ],
+      [ "socx::util::TimerFd", "classsocx_1_1util_1_1_timer_fd.html", null ]
     ] ],
-    [ "socx::util::Process", "classsocx_1_1util_1_1_process.html", null ],
-    [ "socx::util::TimerFd", "classsocx_1_1util_1_1_timer_fd.html", null ]
+    [ "socx::util::Process", "classsocx_1_1util_1_1_process.html", null ]
 ];
